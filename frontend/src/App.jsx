6f8dd2663eefeab61f786b3ba9d4e601ee9f6b25
vikash rathour <112382180+v-rathour@users.noreply.github.com>
2024-04-23 12:56:38 +0530

    Authentication added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import HomePage from './components/HomePage/HomePage'
+import SignUpPage from './components/auth/SignUpPage'
+import LoginPage from './components/auth/LoginPage'
 
 
 const App = () => {
@@ -8,6 +10,8 @@ const App = () => {
     <React.Fragment>
         <Routes>
               <Route path='/' element={<HomePage/> } />   
+              <Route path='/user/signup' element={<SignUpPage/>}  />   
+              <Route path='/user/login' element={<LoginPage/> } />   
         </Routes>
     </React.Fragment>
   )
