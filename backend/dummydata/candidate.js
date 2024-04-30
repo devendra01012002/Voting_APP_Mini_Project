59c4a91507c16da0a3c07207cb2f7bbf23fe3c15
vikash rathour <112382180+v-rathour@users.noreply.github.com>
2024-04-30 11:25:59 +0530

    Add commit

--- backend/dummydata/candidate.js
+++ backend/dummydata/candidate.js
@@ -1,34 +1,34 @@
 [
   {
     name: "Narendra Modi",
-    party: "BJP",
+    party: "Bharatiya Janata Party",
     partyImage: "https://m.media-amazon.com/images/I/61OgrIEL7EL.jpg",
     age: 71,
   },
   {
     name: "Rahul Gandhi",
-    party: "INC",
+    party: "Indian National Congress",
     partyImage:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Indian_National_Congress_hand_logo.svg/1200px-Indian_National_Congress_hand_logo.svg.png",
     age: 51,
   },
   {
     name: "Arvind Kejriwal",
-    party: "AAP",
+    party: "Aam Aadmi Party",
     partyImage:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwECdUJY4gRcw_BPRqF0ZWndbAyS7aQ1Ak-GIWkggyRGofit45fUcjGmwtbRNPIaXZ10Y&usqp=CAU",
     age: 53,
   },
   {
     name: "Mamata Banerjee",
-    party: "AITC",
+    party: "The All India Trinamool Congress",
     partyImage:
       "https://upload.wikimedia.org/wikipedia/commons/9/93/AITC_New_Logo.png",
     age: 67,
   },
   {
     name: "Yogi Adityanath",
-    party: "BJP",
+    party: "Bharatiya Janata Party",
     partyImage: "https://m.media-amazon.com/images/I/61OgrIEL7EL.jpg",
     age: 49,
   },
